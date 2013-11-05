24fd1319e3bff6777c082ee5538153b372fdc9e4
Rafael Oliveira <rafa_so@aol.com>
2013-11-05 00:44:45 -0200

    subscriptions

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,16 +1,14 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+# codig: utf-8
 from django.test import TestCase
 
+class HomepageTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_get(self):
+        'GET / must return status code 200'
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        'Homepage must use template index.html'
+        self.assertTemplateUsed(self.resp, 'index.html')
---
+++ eventex/subscriptions/views.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from django.http import HttpResponse
+
+#def subscribe(resquest):
+#    return HttpResponse
