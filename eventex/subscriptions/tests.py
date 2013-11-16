e1f917d4703dc9b4f99ec9a3f7fc214af9401f8e
Rafael Oliveira <rafa_so@aol.com>
2013-11-16 10:59:15 -0200

    criaçao e teste de formulario para inscricao

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -8,3 +8,18 @@ class SubscribeTest(TestCase):
     def test_get(self):
         'GET /inscricao/ must return status code'
         self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        'Response should be a rendered template'
+        self.assertTemplateUsed(self.resp, 'subscriptions/subscriptions_form.html')
+
+    def test_html(self):
+        'Html must contain input controls'
+        self.assertContains(self.resp, '<form')
+        self.assertContains(self.resp, '<input', 6)
+        self.assertContains(self.resp, 'type="text"', 4)
+        self.assertContains(self.resp, 'type="submit"')
+
+    def test_csrf(self):
+        'Html must contain csrf token'
+        self.assertContains(self.resp, csrfmiddlewaretoken)
